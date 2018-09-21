197ea7d683e9ff35fcd85fc085a4dc347898a8c1
sysevil <sysevil@github.com>
2018-09-20 22:08:20 -0300

    add new functions

--- brmanglerGO.go
+++ brmanglerGO.go
@@ -35,24 +35,39 @@ func validadeInputFile(file string) bool {
 }
 
 func checkPasswdSize(password string, min int, max int) {
+
 	if len(password) >= min && len(password) <= max {
 		fmt.Printf("%s \n", strings.TrimSuffix(password, "\n"))
 	}
 }
 
+func wordSpecial(word string, size int, specialArray []string, min int, max int) {
+
+	for _, value := range specialArray {
+		concatPassword := word + value
+		reverseConcatPassword := value + word
+		checkPasswdSize(concatPassword, min, max)
+		checkPasswdSize(reverseConcatPassword, min, max)
+	}
+
+}
+
 func main() {
+
 	file := flag.String("file", "", "file with names --file /path/to/file")
 	minimumPwdSize := flag.Int("min", 8, "minimum password size")
 	maxPwdSize := flag.Int("max", 12, "max passoword size")
+	special := flag.Bool("s", false, "DISABLE passwords with special characters")
 	//leet := flag.Bool("leet", true, "disable leet speak (e.g. p@55w0rd)")
 	//capitalize := flag.Bool("capitalize", true, "disable capitalize words like (eg Passwords)")
 	//calcOutPut := flag.Bool("calc", false, "Calculate the quantity of outputs")
 	//upcase := flag.Bool("upcase", true, "disable uppercase")
 	//dates := flag.Bool("dates", true, "disable dates (password@022102009)")
-	//special := flag.Bool("special", true, "disable passwords with special chars")
 	//twoLetters := flag.Bool("twoletters", false, "enable two letters passwords ( af212301031)")
 	//insane := flag.Bool("insane", true, "Use *ALL* wordlists to create passwords")
 	flag.Parse()
+
+	SPC := []string{"!", "@", "$", "%", "*"}
 
 	start := time.Now()
 	fmt.Println(start.Format("3:04PM"))
@@ -65,11 +80,14 @@ func main() {
 			log.Fatal("[+] Error open a file")
 		}
 		defer inFile.Close()
-		scanner := bufio.NewScanner(inFile)
-		scanner.Split(bufio.ScanLines)
 
-		for scanner.Scan() {
-			checkPasswdSize(scanner.Text(), *minimumPwdSize, *maxPwdSize)
+		if *special {
+			sizeSpecial := len(SPC)
+			scanner := bufio.NewScanner(inFile)
+			for scanner.Scan() {
+				password := scanner.Text()
+				wordSpecial(password, sizeSpecial, SPC, *minimumPwdSize, *maxPwdSize)
+			}
 		}
 	}
 
